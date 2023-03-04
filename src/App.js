f3626069d3128dece36e3732501545e49d1478e9
Narendra Khariya <khariyanaresh007@gmail.com>
2023-03-05 08:09:18 +0900

    Use async await to make it more simpler ways

--- src/App.js
+++ src/App.js
@@ -20,22 +20,19 @@ function App() {
     },
   ];
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transFormedMovies = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            releaseDate: movie.release_date,
-          };
-        });
-        setMovies(transFormedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+
+    const transFormedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transFormedMovies);
   }
   return (
     <React.Fragment>
